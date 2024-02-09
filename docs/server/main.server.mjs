aa8c6a12d0df1bf23703523836550624b76531e6
Ravi Gangawane <developer.ravi08@gmail.com>
2024-02-09 15:11:07 +0530

    update view

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2H5CMTI5.mjs";import"./chunk-TVEW3AMV.mjs";import"./chunk-DHC35VFN.mjs";import"./chunk-HGQAQ46A.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-ZAOPUB5S.mjs";import"./chunk-TVEW3AMV.mjs";import"./chunk-DHC35VFN.mjs";import"./chunk-HGQAQ46A.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
